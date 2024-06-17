f4e4edac14373e80daf3e48baeded26fb2d21236
MilanoDeveloper <milano.developer@gmail.com>
2024-06-17 12:57:59 -0300

    Components, Courses Service

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,25 @@
+import { CoursesService } from './../services/courses.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent implements OnInit{
+
+  courses: Course[] = [ { _id: "1", name: "Angular", category: "Front-end"}];
+  displayedColumns = ['name', 'category'];
+
+  // coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService){
+   // this.coursesService = new CoursesService();
+  }
+
+  ngOnInit(): void {
+    this.courses = this.coursesService.list();
+  }
+}
