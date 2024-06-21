5b046887b928f5438676d8734a6dcbd8fd40b778
MilanoDeveloper <milano.developer@gmail.com>
2024-06-21 16:49:46 -0300

    Routing changes

--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,20 +1,21 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
+        importProvidersFrom(BrowserModule, MatToolbarModule),
         provideAnimationsAsync(),
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
     ]
 })
   .catch(err => console.error(err));
